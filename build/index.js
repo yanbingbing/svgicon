3ab76f631ee417a7954cea66ac7a758252803ba1
kangwei <bingbing.yb@alibaba-inc.com>
2016-07-21 14:10:31 +0800

    chore:ver 1.0.2

--- build/index.js
+++ build/index.js
@@ -2,18 +2,19 @@
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+function _defaults(obj, defaults) { var keys = Object.getOwnPropertyNames(defaults); for (var i = 0; i < keys.length; i++) { var key = keys[i]; var value = Object.getOwnPropertyDescriptor(defaults, key); if (value && value.configurable && obj[key] === undefined) { Object.defineProperty(obj, key, value); } } return obj; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : _defaults(subClass, superClass); }
 
 require('./index.less');
 var React = require('react');
+var objectAssign = require('object-assign');
 
 function testType(src) {
   var t = typeof src === 'undefined' ? 'undefined' : _typeof(src);
@@ -32,38 +33,35 @@ module.exports = function (store) {
     function Icon() {
       _classCallCheck(this, Icon);
 
-      return _possibleConstructorReturn(this, Object.getPrototypeOf(Icon).apply(this, arguments));
+      return _possibleConstructorReturn(this, _React$Component.apply(this, arguments));
     }
 
-    _createClass(Icon, [{
-      key: 'render',
-      value: function render() {
-        var src = this.props.name,
-            type = testType(src);
+    Icon.prototype.render = function render() {
+      var src = this.props.name,
+          type = testType(src);
 
-        var className = "svgicon svgicon-size-" + this.props.size;
-        if (this.props.className) {
-          className += " " + this.props.className;
-        }
-
-        var props = Object.assign({}, this.props, {
-          className: className,
-          size: null,
-          name: null
-        });
-
-        if (type === 'ReactClass') {
-          return React.createElement(src, props);
-        }
-        if (type === 'ReactElement') {
-          return React.cloneElement(src, props);
-        }
-        if (type === 'string' && (src = store(src))) {
-          return React.createElement(src, props);
-        }
-        return null;
+      var className = "svgicon svgicon-size-" + this.props.size;
+      if (this.props.className) {
+        className += " " + this.props.className;
       }
-    }]);
+
+      var props = objectAssign({}, this.props, {
+        className: className,
+        size: null,
+        name: null
+      });
+
+      if (type === 'ReactClass') {
+        return React.createElement(src, props);
+      }
+      if (type === 'ReactElement') {
+        return React.cloneElement(src, props);
+      }
+      if (type === 'string' && (src = store(src))) {
+        return React.createElement(src, props);
+      }
+      return null;
+    };
 
     return Icon;
   }(React.Component);
@@ -82,29 +80,26 @@ module.exports = function (store) {
     function Button() {
       _classCallCheck(this, Button);
 
-      return _possibleConstructorReturn(this, Object.getPrototypeOf(Button).apply(this, arguments));
+      return _possibleConstructorReturn(this, _React$Component2.apply(this, arguments));
     }
 
-    _createClass(Button, [{
-      key: 'render',
-      value: function render() {
-        var className = "svgicon-button";
-        if (this.props.className) {
-          className += " " + this.props.className;
-        }
-        var contents = this.props.text || this.props.children;
-        return React.createElement(
-          'div',
-          _extends({}, this.props, { className: className, size: null, text: null, name: null }),
-          React.createElement(Icon, { name: this.props.name, size: this.props.size || "small" }),
-          contents && React.createElement(
-            'span',
-            { className: 'svgicon-contents' },
-            contents
-          )
-        );
+    Button.prototype.render = function render() {
+      var className = "svgicon-button";
+      if (this.props.className) {
+        className += " " + this.props.className;
       }
-    }]);
+      var contents = this.props.text || this.props.children;
+      return React.createElement(
+        'div',
+        _extends({}, this.props, { className: className, size: null, text: null, name: null }),
+        React.createElement(Icon, { name: this.props.name, size: this.props.size || "small" }),
+        contents && React.createElement(
+          'span',
+          { className: 'svgicon-contents' },
+          contents
+        )
+      );
+    };
 
     return Button;
   }(React.Component);
